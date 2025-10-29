0f78caad371f71131ca617328881b7ae5e4da5c8
Manuel Jarry <manuel.jarry@polymtl.ca>
2025-10-29 08:24:04 -0400

    build:Added interest.cbl, french-language calculator

---
+++ COBOL/interest.cbl
@@ -0,0 +1,33 @@
+      *****************************************************************
+      * Nom du programme:    INTR0001                               
+      * Auteur original: MANUEL JARRY                                
+      *
+      * Journal d'entretien                                             
+      * Date       Auteur        Exigence de maintenance               
+      * ---------- ------------  --------------------------------------- 
+      * 29/10/2025 MANUEL JARRY  Créé pour classe COBOL       
+      *                                                               
+      *****************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID.  INTR0001.
+       AUTHOR. MANUEL JARRY. 
+       INSTALLATION. COBOL DEVELOPMENT CENTER. 
+       DATE-WRITTEN. 29/10/2025. 
+       DATE-COMPILED. JJ/MM/AA. 
+       SECURITY. NON-CONFIDENTIAL.
+      *****************************************************************
+      *
+       ENVIRONMENT DIVISION.
+      *
+       INPUT-OUTPUT SECTION. 
+      *
+       DATA DIVISION. 
+      *
+       FILE SECTION. 
+      *
+       WORKING-STORAGE SECTION. 
+      *
+       77  FIN-DE-SECTION-SWITCH      PIC 9       VALUE 0.
+       77  VALEUR-ACTUELLE            PIC 9(7)V99.
+       
+
