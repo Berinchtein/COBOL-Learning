0af73fc899c3c558e32bca0793a3c8073dea8d4f
Manuel Jarry <lemanujarry@gmail.com>
2025-10-31 09:09:37 -0400

    feat:Added INTERET-V3, a new version with error loop thanks to GO TO

---
+++ COBOL/INTERET-V3.cbl
@@ -0,0 +1,120 @@
+      *****************************************************************
+      * Nom du programme:    INTR0003
+      * Auteur original: MANUEL JARRY
+      *
+      * Journal d'entretien
+      * Date       Auteur        Exigence de maintenance
+      * ---------- ------------  --------------------------------------- 
+      * 31/10/2025 MANUEL JARRY  Créé pour classe COBOL
+      *
+      *****************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID.  INTR0003.
+       AUTHOR. MANUEL JARRY. 
+       INSTALLATION. COBOL DEVELOPMENT CENTER. 
+       DATE-WRITTEN. 31/10/2025. 
+       DATE-COMPILED. DD/MM/2025. 
+       SECURITY. NON-CONFIDENTIAL.
+      *****************************************************************
+      *
+       ENVIRONMENT DIVISION. 
+      *
+       INPUT-OUTPUT SECTION. 
+      *
+       DATA DIVISION. 
+      *
+       FILE SECTION. 
+      *
+       WORKING-STORAGE SECTION.
+      *
+       01 SWITCHES.
+          05 FIN-DE-SECTION-SWITCH      PIC 9          VALUE 0.
+      *
+       01 ENTREES-UTILISATEUR.
+          05 VALEURS-MONETAIRES.
+             10 VALEUR-ACTUELLE         PIC 9(7)V99.
+          05 VALEURS-AUTRES.
+             10 TAUX-INTERET            PIC 9(2)V99.
+             10 TYPE-TAUX-INTERET       PIC X(9).
+             10 NOMBRE-D-ANNEES         PIC 9(2).
+      *
+       01 VARIABLES-TRAVAIL.
+          05 VALEURS-MONETAIRES.
+             10 VALEUR-FUTURE           PIC 9(7)V99.
+             10 VALEUR-FUTURE-FORMATTEE PIC Z,ZZZ,ZZZ.99.
+      *
+       PROCEDURE DIVISION.
+      *
+       000-CALCUL-TOTAL-INTERET.
+      *
+           PERFORM 100-CALCUL-UN-INTERET
+              UNTIL FIN-DE-SECTION-SWITCH = 1.
+           DISPLAY "FIN DU PROGRAMME.".
+           STOP RUN.
+      *
+       100-CALCUL-UN-INTERET.
+      *
+           DISPLAY "--------------------------------------------------".
+           DISPLAY "POUR TERMINER LE PROGRAMME, ENTRER 0.".
+           DISPLAY "ENTRER LE MONTANT DE L'INVESTISSEMENT.".
+           ACCEPT VALEUR-ACTUELLE.
+           IF VALEUR-ACTUELLE = 0
+              MOVE 1 TO FIN-DE-SECTION-SWITCH
+           ELSE
+              PERFORM 200-MONTRER-VALEUR-FUTURE
+           END-IF.
+      *
+       200-MONTRER-VALEUR-FUTURE.
+           PERFORM 210-DEMANDER-NOMBRE-ANNEES.
+           PERFORM 220-DEMANDER-TAUX-INTERET.
+           PERFORM 230-DEMANDER-TYPE-TAUX-INTERET.
+           IF TYPE-TAUX-INTERET = "ANNUEL"
+              COMPUTE VALEUR-FUTURE =
+                 VALEUR-ACTUELLE *(1 +
+                 TAUX-INTERET / 100) **
+                 NOMBRE-D-ANNEES
+           ELSE
+              IF TYPE-TAUX-INTERET = "MENSUEL"
+                 COMPUTE VALEUR-FUTURE =
+                    VALEUR-ACTUELLE *(1 +
+                    (TAUX-INTERET / 12) / 100) **
+                    (NOMBRE-D-ANNEES * 12)
+              ELSE
+                 COMPUTE VALEUR-FUTURE =
+                    VALEUR-ACTUELLE *(1 +
+                    (TAUX-INTERET / 365) / 100) **
+                    (NOMBRE-D-ANNEES * 365)
+              END-IF
+           END-IF.
+           MOVE VALEUR-FUTURE TO VALEUR-FUTURE-FORMATTEE.
+           DISPLAY "VALEUR-FUTURE = " VALEUR-FUTURE.
+      *
+       210-DEMANDER-NOMBRE-ANNEES.
+           DISPLAY "ENTRER LE NOMBRE D'ANNEES.".
+           ACCEPT NOMBRE-D-ANNEES.
+           IF NOMBRE-D-ANNEES < 0
+              DISPLAY "NOMBRE D'ANNEES INCORRECT. VEUILLEZ REESSAYER."
+              GO TO 210-DEMANDER-NOMBRE-ANNEES
+           END-IF.
+      *
+       220-DEMANDER-TAUX-INTERET.
+           DISPLAY "ENTRER LE TAUX D'INTERET ANNUEL".
+           ACCEPT TAUX-INTERET.
+           IF (TAUX-INTERET < 0 OR TAUX-INTERET > 50)
+              DISPLAY "TAUX D'INTERET ANNUEL INCORRECT."
+                      " VEUILLEZ REESSAYER."
+              GO TO 220-DEMANDER-TAUX-INTERET
+           END-IF.
+      *
+       230-DEMANDER-TYPE-TAUX-INTERET.
+           DISPLAY "ENTRER LE TYPE DE TAUX D'INTERET".
+           DISPLAY "TYPES POSSIBLE: 'ANNUEL', 'MENSUEL' ET 'QUOTIDIEN".
+           ACCEPT TYPE-TAUX-INTERET.
+           IF (TYPE-TAUX-INTERET NOT = "ANNUEL"
+              AND TYPE-TAUX-INTERET NOT = "MENSUEL"
+              AND TYPE-TAUX-INTERET NOT = "QUOTIDIEN")
+              DISPLAY "TYPE DE TAUX D'INTERET INCORRECT."
+                      " VEUILLEZ REESSAYER."
+              GO TO 230-DEMANDER-TYPE-TAUX-INTERET
+           END-IF.
+           
